38cf46698d508e114c30e40475e3077d6c436b66
Emanuel Lewis <emanuellewis51@gmail.com>
2026-06-23 12:53:48 -0400

    fix: add html-loader and update favicon path

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|webp|gif|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource",
       },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
     ],
   },
   plugins: [
